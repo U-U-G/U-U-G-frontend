596b0eb8b8cb305ac960039c31c26ba3e8473c3b
chahyunlee <chahyun023@gmail.com>
2026-04-16 11:44:33 +0900

    refactor: 스타일 디테일 개선

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -16,7 +16,7 @@ export default function Login() {
             <input
               type="email"
               placeholder="이메일"
-              className="w-full border-0 border-b border-foreground-primary bg-transparent px-1 pb-4 pt-2 text-[16px] text-foreground-primary outline-none placeholder:text-secondary"
+              className="w-full border-0 border-b border-secondary bg-transparent px-1 pb-4 pt-2 text-lg text-text-primary outline-none focus:border-text-primary placeholder:text-secondary transition-all duration-300 ease-in-out"
             />
           </div>
 
@@ -24,19 +24,19 @@ export default function Login() {
             <input
               type="password"
               placeholder="비밀번호"
-              className="w-full border-0 border-b border-foreground-primary bg-transparent px-1 pb-4 pt-2 text-[16px] text-foreground-primary outline-none placeholder:text-secondary"
+              className="w-full border-0 border-b border-secondary bg-transparent px-1 pb-4 pt-2 text-lg text-text-primary outline-none focus:border-text-primary placeholder:text-secondary transition-all duration-300 ease-in-out"
             />
           </div>
 
           <button
             type="submit"
-            className="mb-10 h-[56px] w-full rounded-md bg-primary text-[20px] font-bold text-white cursor-pointer"
+            className="mb-10 h-[56px] w-full rounded-md bg-primary text-xl font-bold text-white cursor-pointer"
           >
             로그인
           </button>
         </form>
 
-          <div className="font-medium mb-12 flex items-center justify-center gap-4 text-[16px] text-black">
+          <div className="font-medium mb-12 flex items-center justify-center gap-4 text-md text-text-primary">
             <button className = "cursor-pointer" type="button">비밀번호 찾기</button>
             <span className="text-foreground-primary">|</span>
             <button className = "cursor-pointer" type="button">회원가입</button>
@@ -48,15 +48,15 @@ export default function Login() {
               className="flex h-[68px] w-[68px] items-center justify-center rounded-full bg-[#fee500] cursor-pointer"
               aria-label="카카오 로그인"
             >
-              <img src="/kakao-logo.svg" alt="카카오 로그인" />
+              <img src="/icon/kakao-logo.svg" alt="카카오 로그인" />
             </button>
 
             <button
               type="button"
-              className="flex h-[68px] w-[68px] items-center justify-center rounded-full bg-white shadow-[0_2px_10px_rgba(0,0,0,0.08)] cursor-pointer"
+              className="flex h-[68px] w-[68px] items-center justify-center rounded-full shadow-[0_2px_10px_rgba(0,0,0,0.08)] cursor-pointer"
               aria-label="구글 로그인"
             >
-              <img src="/google-logo.svg" alt="구글 로그인" />
+              <img src="/icon/google-logo.svg" alt="구글 로그인" />
             </button>
 
             <button
@@ -64,7 +64,7 @@ export default function Login() {
               className="flex h-[68px] w-[68px] items-center justify-center rounded-full bg-[#03c75a] cursor-pointer"
               aria-label="네이버 로그인"
             >
-              <img src="/naver-logo.svg" alt="네이버 로그인" />
+              <img src="/icon/naver-logo.svg" alt="네이버 로그인" />
             </button>
           </div>
       </section>
