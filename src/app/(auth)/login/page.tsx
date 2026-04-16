697be58d4286a8cb47d953a5e00bd599b582b52d
chahyunlee <chahyun023@gmail.com>
2026-04-16 12:08:45 +0900

    refactor: svg 파일을 public에서 src/assets로 이동

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,13 @@
 "use client"
 
+import KakaoLogoIcon from "@/assets/icon/kakao-logo.svg"
+import GoogleLogoIcon from "@/assets/icon/google-logo.svg"
+import NaverLogoIcon from "@/assets/icon/naver-logo.svg"
+
 export default function Login() {
 
   const handleLogin = ()=>{
-    console.log("로그인 페이지로 이동") //TODO: 로그인 api 연동
+    console.log("홈으로 이동") //TODO: 로그인 api 연동
   }
 
   return (
@@ -48,7 +52,7 @@ export default function Login() {
               className="flex h-[68px] w-[68px] items-center justify-center rounded-full bg-[#fee500] cursor-pointer"
               aria-label="카카오 로그인"
             >
-              <img src="/icon/kakao-logo.svg" alt="카카오 로그인" />
+              <img src={KakaoLogoIcon} alt="카카오 로그인" />
             </button>
 
             <button
@@ -56,7 +60,7 @@ export default function Login() {
               className="flex h-[68px] w-[68px] items-center justify-center rounded-full shadow-[0_2px_10px_rgba(0,0,0,0.08)] cursor-pointer"
               aria-label="구글 로그인"
             >
-              <img src="/icon/google-logo.svg" alt="구글 로그인" />
+              <img src={GoogleLogoIcon} alt="구글 로그인" />
             </button>
 
             <button
@@ -64,7 +68,7 @@ export default function Login() {
               className="flex h-[68px] w-[68px] items-center justify-center rounded-full bg-[#03c75a] cursor-pointer"
               aria-label="네이버 로그인"
             >
-              <img src="/icon/naver-logo.svg" alt="네이버 로그인" />
+              <img src={NaverLogoIcon} alt="네이버 로그인" />
             </button>
           </div>
       </section>
