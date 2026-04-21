706afd5129e8c4a340f4513077eb788faa176dde
dohyeon0709 <dohy0709@naver.com>
2026-04-21 20:47:11 +0900

    feat : 면접 연습 페이지 추가

---
+++ src/app/interview/page.tsx
@@ -0,0 +1,9 @@
+import PracticeMethodSection from '@/components/interview/PracticeMethodSection'
+
+export default function InterviewPage() {
+  return (
+    <main className="flex-1 pl-14.25 pt-7">
+      <PracticeMethodSection />
+    </main>
+  )
+}
