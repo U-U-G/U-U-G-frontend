e5ca896416d3f9f635e95f4bc2ce213934a84f69
dohyeon0709 <dohy0709@naver.com>
2026-04-23 15:11:36 +0900

    Revert "feat : 면접 연습 페이지 추가"
    This reverts commit 706afd5129e8c4a340f4513077eb788faa176dde.

--- src/app/interview/page.tsx
+++
@@ -1,9 +0,0 @@
-import PracticeMethodSection from '@/components/interview/PracticeMethodSection'
-
-export default function InterviewPage() {
-  return (
-    <main className="flex-1 pl-14.25 pt-7">
-      <PracticeMethodSection />
-    </main>
-  )
-}
