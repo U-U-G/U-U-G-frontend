11ad659d0d5ceb2a4aaa1bde684a6853d80d7c36
chahyunlee <chahyun023@gmail.com>
2026-05-08 00:14:20 +0900

    feat: 커리큘럼 API 클라이언트 구현

---
+++ src/apis/schedules/index.tsx
@@ -0,0 +1,48 @@
+import { privateClient } from '@/apis/common/privateClient'
+import { ApiResponse } from '../common/type'
+import type {
+  ScheduleDetail,
+  CreateScheduleRequest,
+  UpdateScheduleRequest,
+  GetScheduleListResponse,
+} from '@/apis/schedules/type'
+
+export const createInterviewSchedule = async (body: CreateScheduleRequest) => {
+  const { data } = await privateClient.post<ApiResponse<ScheduleDetail>>(
+    '/api/schedules',
+    body,
+  )
+  return data.data
+}
+
+export const getInterviewScheduleList = async () => {
+  const { data } =
+    await privateClient.get<ApiResponse<GetScheduleListResponse>>(
+      '/api/schedules',
+    )
+  return data.data
+}
+
+export const getInterviewSchedule = async (scheduleUuid: string) => {
+  const { data } = await privateClient.get<ApiResponse<ScheduleDetail>>(
+    `/api/schedules/${scheduleUuid}`,
+  )
+  return data.data
+}
+
+export const updateInterviewSchedule = async (
+  scheduleUuid: string,
+  body: UpdateScheduleRequest,
+) => {
+  const { data } = await privateClient.put<ApiResponse<ScheduleDetail>>(
+    `/api/schedules/${scheduleUuid}`,
+  )
+  return data.data
+}
+
+export const deleteInterviewSchedule = async (scheduleUuid: string) => {
+  const { data } = await privateClient.delete<ApiResponse<string>>(
+    `/api/schedules/${scheduleUuid}`,
+  )
+  return data.data
+}
---
+++ src/apis/schedules/type.d.ts
@@ -0,0 +1,26 @@
+export interface Curriculum {
+  uuid: string
+  jobPostingUuid: string
+  content: string
+  scheduledDate: string
+  isPast: boolean
+}
+
+export interface Schedule {
+  scheduleUuid: string
+  companyName: string
+  interviewDate: string
+}
+
+export interface ScheduleDetail extends Schedule {
+  curriculums: Curriculum[]
+}
+
+export interface CreateScheduleRequest {
+  companyName: string
+  interviewDate: string
+}
+
+export type UpdateScheduleRequest = CreateScheduleRequest
+
+export type GetScheduleListResponse = Schedule[]
