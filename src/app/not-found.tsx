43e40433d3a0f86d94f7f600e865e02c48d66cd7
dohyeon0709 <dohy0709@naver.com>
2026-05-09 22:27:12 +0900

    refactor : PR 코드리뷰 반영

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -25,7 +25,7 @@ export default function NotFound() {
           />
         </div>
 
-        <div className="text-center mb-12 font-light">
+        <div className="text-center mb-12">
           <p className="h1 text-primary mb-2">페이지를 찾을 수 없습니다</p>
           <p className="h3 text-text-primary">
             요청하신 페이지가 존재하지 않거나 이동되었을 수 있습니다
